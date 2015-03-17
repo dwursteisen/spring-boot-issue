4f42b7505573d27dfa25a15cc46656f170c61293
David Wursteisen <david.wursteisen+git@gmail.com>
2015-03-17 17:48:01 +0100

    Result

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by david.wursteisen on 17/03/2015.
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        return new ModelAndView("index", "hello", "Héllo le Wörld");
     }
 }
